a548166d4045deabd8910bde12ddbf53ea2ab354
Rui Li <rli3@caltech.edu>
2022-10-19 14:55:49 -0700

    clean simple_mixing

--- src/mixing/simple_mixing.h
+++ src/mixing/simple_mixing.h
@@ -12,11 +12,10 @@ struct simple_mixing {
   std::pair<scf::Update<State>, simple_mixing> operator()(const State & state) {
     simple_mixing<State> new_mixer{alpha, state};
     const auto & old_state = state;
-    const double alpha_in_function = alpha;
-    const scf::Update<State> updater = [old_state, alpha_in_function]
+    const scf::Update<State> updater = [old_state, alpha = this->alpha]
         (const State & state_in_function) -> State {
-      const State left = alpha_in_function * state_in_function;
-      const State right = (1.0 - alpha_in_function) * old_state;
+      const State left = alpha * state_in_function;
+      const State right = (1.0 - alpha) * old_state;
       return left + right;
     };
     return  {updater, new_mixer};
