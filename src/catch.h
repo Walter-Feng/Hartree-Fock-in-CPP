32abcf72f7ba1877b6dec4cd4819f3559d4a68dd
Rui Li <rli3@caltech.edu>
2022-11-08 16:22:55 -0800

    update catch2

---
+++ src/catch.h
@@ -0,0 +1,8 @@
+#ifndef HFINCPP_CATCH_H
+#define HFINCPP_CATCH_H
+
+#include <catch2/catch_test_macros.hpp>
+#include <cstdint>
+#define CATCH_CONFIG_MAIN
+
+#endif //HFINCPP_CATCH_H
--- src/integral/rys_quadrature_unit_test.cpp
+++ src/integral/rys_quadrature_unit_test.cpp
@@ -1,4 +1,4 @@
-#include <catch.hpp>
+#include "catch.h"
 
 #include "rys_quadrature.h"
 
